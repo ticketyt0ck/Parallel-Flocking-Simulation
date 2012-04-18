8689885727eb2d84b68e93282beb34c5afa4f296
Nick Yelito <stnay01@moravian.edu>
2012-04-18 15:03:49 -0400

    added stuff to read data in from command line

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 // Main class for running the parallel flocking sim
 
 int main(int argc, char **argv) {
@@ -6,15 +7,21 @@ int main(int argc, char **argv) {
 	long num_agents;
 	long num_steps;
 	char* filename;
-	
-	if (argc != 3) {
+
+	if (argc != 4) {
         printf("Usage: num_agents num_steps filename\n");
         exit(1);
         }
         
         num_agents = atol(argv[1]);
         num_steps = atol(argv[2]);
+        filename = argv[3];
         
         
         
-        //printf("Running a simulation 
+        printf("Running a simulation with %lu agents for %lu steps and saving the data to the file %s\n", 
+        	num_agents, num_steps, filename);
+        
+        return 0;
+        
+}
