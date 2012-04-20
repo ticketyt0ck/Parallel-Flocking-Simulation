715a56e97baedfdaadd2d521a2170983e1e974f3
Nick Yelito <stnay01@moravian.edu>
2012-04-19 22:49:56 -0400

    added work to build a list or randomly placed agents, not finished

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "agent.h"
+#include "vector.h"
+#include <sys/time.h>
 // Main class for running the parallel flocking sim
+
+#define WINDOWSIZE 500
 
 int main(int argc, char **argv) {
 	
 	long num_agents;
 	long num_steps;
 	char* filename;
+	long timestep;
 
 	if (argc != 4) {
         printf("Usage: num_agents num_steps filename\n");
@@ -26,8 +32,31 @@ int main(int argc, char **argv) {
         
         
         // create the agents, with random headings, maybe random locations or all in a bunch
+        int i;
+        Agent agent_list[num_agents];
+        
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        
+        srand(tv.tv_usec);
+        for(i = 0; i < num_agents - 1; i++){
+        	Vector2D heading = init_vector((rand_r(i) % 5), (rand_r(i) % 5));
+        	Point2D start_loc = init_point((rand_r(i) % WINDOWSIZE), (rand_r(i) % WINDOWSIZE));
+        	double speed = 5;
+        	agent_list[i] = init_agent(heading, start_loc, speed, Agent_Type prey);
+        	
+        	
+        }
+        Vector2D heading = init_vector((rand_r(i) % 5), (rand_r(i) % 5));
+        Point2D start_loc = init_point((rand_r(i) % WINDOWSIZE), (rand_r(i) % WINDOWSIZE));
+        double speed = 5;
+        agent_list[i] = init_agent(heading, start_loc, speed, Agent_Type predator);
+        
+        
         
         // for each numstep
+        for(timestep = 0; i < num_steps; i++){
+        	
         
         //	for each agent
         //		do cohesion, alignment, and seperation
@@ -41,8 +70,11 @@ int main(int argc, char **argv) {
         //
         //	(if it's not the last step)
         //	write -2 to file
+        
+        }
         //
         // write -1 to file
+        
         
         
         
